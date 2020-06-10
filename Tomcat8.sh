777d10566d3d704512f1af4b1be2aed5ecf9ea4f
rayeesm <66748548+rayees-m@users.noreply.github.com>
2020-06-10 16:11:38 -0400

    Create Tomcat8.sh

---
+++ Tomcat8.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+#Script made for Application installtion
+#Author: Rayees.M
+#Usage: Nginx, Java, PhP, OpenSSL, Gcc, Ulimit for portal installation
+#Distro : Linux -Centos, Rhel, and any fedora
+#Check whether root user is running the script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Update yum repos.and install development tools
+echo "Starting installation of Portal..."
+sudo yum update -y
+sudo yum groupinstall "Development Tools" -y
+sudo yum install screen -y
+
+# Installing needed dependencies and setting ulimit
+echo "Installing  needed dependencies for Portal..."
+sudo yum install  gcc openssl openssl-devel pcre-devel git unzip wget -y
+sudo sed -i '61 i *	soft	nofile	99999' /etc/security/limits.conf
+sudo sed -i '62 i *	hard	nofile	99999' /etc/security/limits.conf
+sudo sed -i '63 i *	soft	noproc	20000' /etc/security/limits.conf
+sudo sed -i '64 i *	hard	noproc	20000' /etc/security/limits.conf
+echo "fs.file-max=6816768" >> /etc/sysctl.conf
+sudo sysctl -w fs.file-max=6816768
+sudo sysctl -p
+
+# Remi-Repo for mysql and php
+echo "Installing the Remi Repo..."
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm && rpm -Uvh epel-release-latest-6.noarch.rpm
+sudo sed -i 's/enabled=0/enabled=1/g' /etc/yum.repos.d/remi.repo
+sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+# Yum update with new repo
+sudo yum update -y
+
+echo "Installing mysql with database...."
+
+# Install MySQL v5
+echo "Installing MySQL..."
+sudo yum install -y mysql mysql-server
+echo "Configuring MySQL data-dir..."
+sudo sed -i /datadir/d /etc/my.cnf
+sudo sed -i '4 i datadir=/var/lib/mysql' /etc/my.cnf
+sudo /etc/init.d/mysqld restart
+# password for root user of mysql
+read -p "Please Enter the Password for New User root : " pass
+sudo /usr/bin/mysqladmin -u root password "$pass"
+
+sleep 2
+#ask user about username
+read -p "Please enter the username you wish to create : " username
+#ask user about password
+read -p "Please Enter the Password for New User ($username) : " password
+
+#mysql query that will create new user, grant privileges on database with entered password
+mysql -uroot -p"$pass" -e "GRANT ALL PRIVILEGES ON dbname.* TO '$username'@'localhost' IDENTIFIED BY '$password'"
+
+echo "Installed MySQL & update new user completed..."
+
+# Installing Java8
+cd /apps/
+echo "Downloading & Installing  Java8..."
+wget https://s3.amazonaws.com/zubron-server-1.0/Java8.zip
+sudo unzip Java8.zip
+sudo alternatives --install /usr/bin/java java /apps/java8/bin/java 1
+sudo alternatives --config java
+sudo alternatives --install /usr/bin/jar jar /apps/java8/bin/jar 1
+sudo alternatives --install /usr/bin/javac javac /apps/java8/bin/javac 1
+sudo alternatives --set jar /apps/java8/bin/jar
+sudo alternatives --set javac /apps/java8/bin/javac
+sudo /apps/java8/bin/java -version
+sudo java –version
+echo '#JAVA Path Setting
+export JAVA_HOME=/apps/java8
+export JRE_HOME=/apps/java8/jre
+export PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin' > /etc/profile.d/java_path.sh
+chmod 755 /etc/profile.d/java_path.sh
+source /etc/profile.d/java_path.sh
+
+# Install Nginx v1.9
+cd /apps/
+echo "Installing Nginx from source..."
+wget http://nginx.org/download/nginx-1.9.9.tar.gz
+sudo tar -zxvf nginx-1.9.9.tar.gz
+mv nginx-1.9.9 nginx
+cd nginx
+sudo mkdir /apps/nginx/logs/
+sudo /bin/bash configure --prefix=/apps/nginx --sbin-path=/apps/nginx/sbin/nginx --conf-path=/apps/nginx/conf/nginx.conf --pid-path=/apps/nginx/logs/nginx.pid --lock-path=/apps/nginx/logs/nginx.lock --with-http_stub_status_module --error-log-path=/apps/nginx/logs/error.log --http-log-path=/apps/nginx/logs/access.log --with-http_ssl_module --http-client-body-temp-path=/apps/nginx/body
+sudo make
+sudo make install
+useradd -r nginx
+
+# configuring Nginx with help of sed
+echo "Configuring Nginx Conf..."
+sudo sed -i "s/mime.types/apps/nginx/conf/g" /apps/nginx/conf/nginx.conf
+sudo sed -i "5 i error_log   /apps/nginx/logs/error.log;" /apps/nginx/conf/nginx.conf
+sudo sed -i "26 i access_log   /apps/nginx/logs/access.log;" /apps/nginx/conf/nginx.conf
+sudo wget https://s3-eu-west-1.amazonaws.com/moofwd-devops/scripts/nginx_init
+sudo mv /apps/nginx_init /etc/init.d/nginx
+sudo chmod 755 /etc/init.d/nginx
+sudo chkconfig --add nginx
+sudo chkconfig --level 345 nginx on
+
+#Cleaning /apps path
+sudo rm -rf /apps/nginx-1.9.9.tar.gz
+sudo rm -rf /apps/Java8.zip
+
+##Now lets install Our Portal...
+echo "Installing Portal & Configuration..."
+sudo groupadd tomcat
+sudo mkdir /apps/tomcat8
+sudo useradd -s /bin/nologin -g tomcat -d /apps/tomcat8 tomcat
+
+cd /apps
+wget http://www-us.apache.org/dist/tomcat/tomcat-8/v8.0.33/bin/apache-tomcat-8.0.33.tar.gz
+sudo tar -zxvf apache-tomcat-8.0.33.tar.gz -C /apps/tomcat8 --strip-components=1
+cd /apps/tomcat8
+sudo chgrp -R tomcat conf && sudo chmod g+rwx conf && sudo chmod g+r conf/* && sudo chown -R tomcat logs/ temp/ webapps/ work/ && sudo chgrp -R tomcat bin && sudo chgrp -R tomcat lib && sudo chmod g+rwx bin && sudo chmod g+r bin/*
+echo '#!/bin/bash
+# Tomcat8: Start/Stop Tomcat 8
+#
+# chkconfig: - 90 10
+# description: Tomcat is a Java application Server.
+. /etc/init.d/functions
+. /etc/sysconfig/network
+CATALINA_HOME=/apps/tomcat8
+TOMCAT_USER=tomcat8
+LOCKFILE=/var/lock/subsys/tomcat8
+RETVAL=0
+start(){
+    echo "Starting Tomcat8: "
+    su - $TOMCAT_USER -c "$CATALINA_HOME/bin/startup.sh"
+    RETVAL=$?
+    echo
+    [ $RETVAL -eq 0 ] && touch $LOCKFILE
+    return $RETVAL
+}
+stop(){
+    echo "Shutting down Tomcat8: "
+    $CATALINA_HOME/bin/shutdown.sh
+    RETVAL=$?
+    echo
+    [ $RETVAL -eq 0 ] && rm -f $LOCKFILE
+    return $RETVAL
+}
+case "$1" in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    restart)
+        stop
+        start
+        ;;
+    *)
+        echo $"Usage: $0 {start|stop|restart}"
+        exit 1
+        ;;
+esac
+exit $?' >> /etc/init.d/tomcat8
+sudo chkconfig --add tomcat8
+sudo chkconfig tomcat8 on
+sudo /etc/init.d/tomcat8 start
